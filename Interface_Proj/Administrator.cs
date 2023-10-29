b35d271da4a33264ff94ed9d35fa516ba9617f0d
Zhanna <zhanigorch@gmail.com>
2023-10-29 15:15:34 +0200

    Додавання студентів

--- Interface_Proj/Administrator.cs
+++ Interface_Proj/Administrator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PasswordLoginGeneration;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Interface_Proj
 {
@@ -28,13 +30,42 @@ namespace Interface_Proj
             //    IAdminInfoStudLB.Items.Add(IAdminInfoStudTB.Text.Trim());
             //   IAdminInfoStudTB.Text = string.Empty;
             //}
-            string newtext = "";
-            if (IAdminInfoStudTB.Text != "" & IAdminInfoStudGenTB != "")
-            {
-                newtext = IAdminInfoStudTB.Text + IAdminInfoStudGenTB;
-            }
-            Methods.AddStudent();
 
+
+            string newtext = "";
+            if (IAdminInfoStudTB.Text != "" && IAdminInfoStudGenTB.Text != "")
+            {
+                newtext = IAdminInfoStudTB.Text + IAdminInfoStudGenTB.Text;
+                //string[] fields = newtext.Split(new[] { ' ', ';' });
+                string[] fields = newtext.Split(new[] { ' ', ';' }, StringSplitOptions.RemoveEmptyEntries);
+                string firstName = "";
+                string lastName = "";
+                string email = "";
+                string group = "";
+                string studentType = "";
+                string login = "";
+                string password = "";
+                if (fields.Length == 7)
+                {
+                    firstName = fields[1];
+                    lastName = fields[2];
+                    email = fields[3];
+                    group = fields[4];
+                    studentType = fields[5];
+                    login = fields[6];
+                    password = fields[7];
+                    Methods.AddStudent(new Student { FirstName = firstName, LastName = lastName, Email = email, Group = group }, new LoginInfo { Login = login, Password = password });
+                    IAdminInfoStudLB.Items.Add(newtext);
+                    IAdminInfoStudTB.Text = string.Empty;
+                    IAdminInfoStudGenTB.Text = string.Empty;
+                }
+                else
+                {
+                    MessageBox.Show("Ви повинні заповнити всі поля інформації про студента");
+                }
+                Methods.ReadStudentsFromCsv();
+               // 
+            }
         }
 
         private void IAdminInfoDelete_Click(object sender, EventArgs e)
@@ -53,7 +84,7 @@ namespace Interface_Proj
                 secondWord = words[1];
             }
             Methods.RemoveStudent(firstWord, secondWord);
-
+            Methods.ReadStudentsFromCsv(); /////
         }
 
         private void IAdminInfoLB_MouseUp(object sender, MouseEventArgs e)
@@ -89,15 +120,34 @@ namespace Interface_Proj
 
         private void IAdminInfoGenerateStudBut_Click(object sender, EventArgs e)
         {
-            //HashTable passwordGenerator = new HashTable();
-
-            //string username = GenerateUsername();
-            //string password = GeneratePassword();
-
-            // Вивести результат в TextBox
-            //IAdminInfoStudGenTB.Text = $"{username}; {password}";
+            string password = GeneratePassword();
+            string nickname = GenerateUsername();
+            IAdminInfoStudGenTB.Text = $"{nickname}; {password}";
         }
 
+        private static Random random = new Random();
+
+        // Username generation
+        static string GenerateUsername(int length = 10)
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._";
+            char[] username = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                username[i] = chars[random.Next(chars.Length)];
+            }
+            return new string(username);
+        }
+        static string GeneratePassword(int length = 8)
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+            char[] password = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                password[i] = chars[random.Next(chars.Length)];
+            }
+            return new string(password);
+        }
         private void IAdminInfoStudLB_SelectedIndexChanged(object sender, EventArgs e)
         {
 
